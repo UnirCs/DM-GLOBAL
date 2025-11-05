ddbb8ebe279cd5e9db7a5c1d6db5473b6f471ff6
Jesus PM - UNIR <jesus.perezmelero@unir.net>
2025-11-05 18:51:35 +0100

    Typo

--- Tema_8/Codigo de apoyo/unir-cinema/App.js
+++ Tema_8/Codigo de apoyo/unir-cinema/App.js
@@ -4,7 +4,7 @@ import "./global.css";
 import {useFonts} from "expo-font";
 import CinemaButton from "./components/CinemaButton";
 import * as Haptics from "expo-haptics";
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
 
@@ -15,23 +15,25 @@ export default function App() {
     if (!fontsLoaded && !error) return null;
 
     return (
-        <SafeAreaView>
-            <View className="px-10 mt-5 bg-white-500 text-white">
-                <StatusBar style="dark"/>
-                <Text className="text-center font-rasa-light text-4xl mb-10">UNIR Cinema</Text>
-                <Text className="text-center font-rasa-light text-2xl mb-10">¿A qué cine deseas ir?</Text>
+        <SafeAreaProvider>
+            <StatusBar style="dark" backgroundColor="aquamarine"/>
+            <SafeAreaView className="flex-1 bg-gray-200">
+                <View className="px-10 mt-5 bg-white-500 text-white">
+                    <Text className="text-center font-rasa-light text-4xl mb-10">UNIR Cinema</Text>
+                    <Text className="text-center font-rasa-light text-2xl mb-10">¿A qué cine deseas ir?</Text>
 
-                <CinemaButton onPress={() => {
-                    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
-                    console.log("Cine de Madrid");}
-                } onLongPress={() => console.log("Cine de Madrid - LongPress")}>
-                    Madrid
-                </CinemaButton>
-                <CinemaButton onPress={() => console.log("Cine de Barcelona")}
-                              onLongPress={() => console.log("Cine de Barcelona - LongPress")}>
-                    Barcelona
-                </CinemaButton>
-            </View>
-        </SafeAreaView>
+                    <CinemaButton onPress={() => {
+                        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+                        console.log("Cine de Madrid");}
+                    } onLongPress={() => console.log("Cine de Madrid - LongPress")}>
+                        Madrid
+                    </CinemaButton>
+                    <CinemaButton onPress={() => console.log("Cine de Barcelona")}
+                                  onLongPress={() => console.log("Cine de Barcelona - LongPress")}>
+                        Barcelona
+                    </CinemaButton>
+                </View>
+            </SafeAreaView>
+        </SafeAreaProvider>
     );
 }
